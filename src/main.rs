eba408eca6536f2c16656801224ad43c017216a6
Pierre Chevalier <pierrechevalier83@gmail.com>
2018-08-05 15:00:10 +0100

    Take input and open image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate clap;
+extern crate image;
+
+use clap::{App, Arg};
+use image::GenericImage;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("spyglass")
+        .version("0.1")
+        .about("renders an image into unicode")
+        .author("Pierre Chevalier")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+    let img_path = matches.value_of("INPUT").unwrap();
+    let img = image::open(img_path).unwrap();
+    println!("dimensions {:?}", img.dimensions());
 }
