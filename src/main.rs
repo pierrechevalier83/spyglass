dd1e53dbd0c8479791ceeb58853f12dc36979f22
Pierre Chevalier <pierrechevalier83@gmail.com>
2018-08-05 15:20:24 +0100

    Use ansi_term

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+extern crate ansi_term;
 extern crate clap;
 extern crate image;
 
 use clap::{App, Arg};
 use image::GenericImage;
+
+mod unicode {
+    pub const SQUARE: char = '█';
+}
 
 fn main() {
     let matches = App::new("spyglass")
@@ -19,4 +24,8 @@ fn main() {
     let img_path = matches.value_of("INPUT").unwrap();
     let img = image::open(img_path).unwrap();
     println!("dimensions {:?}", img.dimensions());
+    println!(
+        "{}",
+        ansi_term::Colour::RGB(70, 130, 180).paint(unicode::SQUARE.to_string())
+    )
 }
