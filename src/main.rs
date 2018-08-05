84a4e4773452ccb3276527397fdb4e1344fce0fe
Pierre Chevalier <pierrechevalier83@gmail.com>
2018-08-05 19:01:03 +0100

    Use some input from real image

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ extern crate clap;
 extern crate image;
 extern crate termsize;
 
+use ansi_term::Colour::RGB;
 use clap::{App, Arg};
-use image::GenericImage;
+use image::{GenericImage, Pixel};
 
 mod unicode {
     pub const SQUARE: char = '█';
@@ -23,16 +24,19 @@ fn main() {
         )
         .get_matches();
     let img_path = matches.value_of("INPUT").unwrap();
-    let img = image::open(img_path).unwrap();
-    println!("dimensions {:?}", img.dimensions());
-
+    let mut img = image::open(img_path).unwrap();
+    let img_dims = img.dimensions();
     let dims = termsize::get().unwrap();
-    for _row in 0..dims.rows - 1 {
-        for _col in 0..dims.cols {
-            print!(
-            "{}",
-            ansi_term::Colour::RGB(70, 130, 180).paint(unicode::SQUARE.to_string())
-    )
+
+    let char_width = img_dims.0 / (dims.rows - 1) as u32;
+    let char_height = img_dims.1 / dims.cols as u32;
+    println!("{}, {}", char_width, char_height);
+
+    for row in 0..dims.rows as u32 - 1 {
+        for col in 0..dims.cols as u32 {
+            let sub = img.sub_image(row * char_width, col * char_height, char_width, char_height).to_image();
+            let rgb = sub.get_pixel(0, 0).to_rgb();
+            print!("{}", RGB(rgb[0], rgb[1], rgb[2]).paint(unicode::SQUARE.to_string()))
         }
         println!("");
     }
